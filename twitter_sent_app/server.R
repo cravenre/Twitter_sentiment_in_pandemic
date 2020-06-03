8cf4003f1032d32ccfe1cde9795821da5f27c3b3
Ryan Craven <ryan@Ryans-MacBook-Pro-2.local>
2020-06-02 21:42:22 -0500

    end of class 6-2-20

--- twitter_sent_app/server.R
+++ twitter_sent_app/server.R
@@ -30,7 +30,10 @@ shinyServer(
                 labs(fill='Density of Tweets')+
                 scale_x_date(date_breaks = "1 week", date_labels = "%b-%d",
                              limits=as.Date(c("2020-01-01 00:00", "2020-05-01 00:00")))+
-                theme(axis.text.x=element_text(angle=90,hjust=1)) +
+                theme(axis.text.x=element_text(angle=90,hjust=1, size=12),
+                      axis.text.y=element_text(size=12),
+                      axis.title.x=element_text(size=16),
+                      axis.title.y=element_text(size=16)) +
                 labs(x='Date of Tweet (2020)', y='Sentiment Score')
         })
         
@@ -41,35 +44,11 @@ shinyServer(
                 labs(x='Week (2020)', y='Tweet Volume')+
                 scale_x_date(date_breaks = "1 week", date_labels = "%b-%d",
                              limits=as.Date(c("2020-01-01 00:00", "2020-05-01 00:00")))+
-                theme(axis.text.x=element_text(angle=90,hjust=1)) +
+                theme(axis.text.x=element_text(angle=90,hjust=1, size=12),
+                      axis.text.y=element_text(size=12),
+                      axis.title.x=element_text(size=16),
+                      axis.title.y=element_text(size=16)) +
                 labs(x='Date of Tweet (2020)', y='Number of Tweets')
         })
-        
-        # sent_plot <- ggplot(df, aes(x=as.Date(created_at), y=sentiment) ) +
-        #     stat_density_2d(data=df[sample(nrow(df), 10000), ], aes(fill = ..level..), geom = "polygon")+
-        #     stat_summary(aes(x = week_set_day, y = sentiment), 
-        #                  fun.x = function(y) length(y) / length(unique(y)), 
-        #                  geom = "line", color='darkblue', size=1) +
-        #     scale_fill_distiller(palette=4, direction=1) +
-        #     labs(fill='Density of Tweets')+
-        #     scale_x_date(date_breaks = "1 week", date_labels = "%b-%d",
-        #                  limits=as.Date(c("2020-01-01 00:00", "2020-05-01 00:00")))+
-        #     theme(axis.text.x=element_text(angle=90,hjust=1)) +
-        #     labs(x='Date of Tweet (2020)', y='Sentiment Score')
-        # 
-        # vol_plot <- ggplot(df, aes(x = week_set_day)) +
-        #     geom_bar(fill = 'darkgreen') +
-        #     labs(x='Week (2020)', y='Tweet Volume')+
-        #     scale_x_date(date_breaks = "1 week", date_labels = "%b-%d",
-        #                  limits=as.Date(c("2020-01-01 00:00", "2020-05-01 00:00")))+
-        #     theme(axis.text.x=element_text(angle=90,hjust=1)) +
-        #     labs(x='Date of Tweet (2020)', y='Number of Tweets')
-        # 
-        # g1grob <- ggplotGrob(sent_plot)
-        # g2grob <- ggplotGrob(vol_plot)
-        # 
-        # output$plots <- renderPlot({
-        #     grid.arrange(g1grob, g2grob)
-        # })
         
 })
