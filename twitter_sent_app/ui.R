17d3b685bfec46e92d62e8cb28c7336f9aca683e
Ryan Craven <ryan@Ryans-MacBook-Pro-2.local>
2020-06-04 08:21:58 -0500

    6-4-20

--- twitter_sent_app/ui.R
+++ twitter_sent_app/ui.R
@@ -11,20 +11,20 @@ shinyUI(
                               column(9,
                                      h2("Changing Public Sentiment During Pandemic"),
                                      h4("COVID-19 has had a large and inescapable impact on all of our lives, both personally
-                                        and professionally. The result of that upheaval is that many people's viewpoints of
-                                        the world around them has changed during that time. As a healthcare worker, I have
-                                        felt that change directly as I have gone from an unknown career to one that is now
-                                        celebrated. I wanted to study how society's viewpoints have changed to other aspects
-                                        of life around them, and how that viewpoint has changed over time during the COVID-19
-                                        pandemic. Because we are a digital society, and because lockdowns, quaranties, and
+                                        and professionally. As a result of that upheaval, many people's viewpoints of
+                                        the world around them have changed during that time. As a healthcare worker, I have
+                                        felt that change directly. I have gone from an little known role to one that is now
+                                        recognized and celebrated. My goal was to study societal viewpoints toward other aspects
+                                        of life and how those have changed over time during the COVID-19
+                                        pandemic. Because we are a digital society, and because lockdowns, quarantines, and
                                         stay-at-home orders have forced us to live remotely, I decided to look at public opinion
-                                        through the lens of social media. I will look at the public sentiment as measured by the
+                                        through the lens of social media. I will look at public sentiment as measured by the
                                         content of Twitter posts because they are short, quick ways for people to express what
                                         they are thinking, feeling, and experiencing."),
                                      br(),
                                      h2("Methods and Workflow"),
                                      h4("Over 70 million tweets were downloaded spanning from December 1, 2019 to April 30,
-                                        2020. The tweets were found in 3 separate datasets. One only contained tweet ID rather
+                                        2020. The tweets were found in 3 separate datasets. One contained only tweet IDs rather
                                         than the full tweet data for privacy reasons. These tweets had to be hydrated using
                                         twarc with a Twitter API and was done in chunks due to the size of the data files. The
                                         tweet ID's were chunked in a random order to ensure that each chunk contained a representative
